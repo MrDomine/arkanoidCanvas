38568ba816bb189d14bf00d83faba560712387e4
MrDomine <d.d.dominus1989@gmail.com>
2022-02-28 15:13:38 +0100

    generacion de ladrillos de forma dinamica

---
+++ js/arkanoid.js
@@ -0,0 +1,179 @@
+window.onload=()=>{
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    var dx = 0; dy = 0;
+    var ballRadius = 10;
+    var paddleHeight = 10;
+    var paddleWidth = 75;
+    var paddleX = (canvas.width-paddleWidth)/2;
+    var rightPressed = false;
+    var leftPressed = false;
+    var x = canvas.width/2;y = canvas.height-paddleHeight-ballRadius;
+
+    var brickRowCount = 3;
+    var brickColumnCount = 5;
+    var brickWidth = 75;
+    var brickHeight = 20;
+    var brickPadding = 10;
+    var brickOffsetTop = 30;
+    var brickOffsetLeft = 30;
+
+    var score = 0;
+    var lives = 3;
+    var inicioJuego = true;
+    var contador = 0;
+
+    document.addEventListener("keydown", keyDownHandler, false);
+    document.addEventListener("keyup", keyUpHandler, false);
+
+    function keyDownHandler(e) {
+        if(e.keyCode == 39) {
+            rightPressed = true;
+        }
+        else if(e.keyCode == 37) {
+            leftPressed = true;
+        } else if(e.keyCode==32 && inicioJuego){
+            dx=-2;
+            dy=-2;
+            inicioJuego=false;
+        }
+    }
+    
+    function keyUpHandler(e) {
+        if(e.keyCode == 39) {
+            rightPressed = false;
+        }
+        else if(e.keyCode == 37) {
+            leftPressed = false;
+        }
+    }
+
+    function collisionDetection() {
+        for(c=0; c<bricks.length; c++) {
+            for(r=0; r<bricks[c].length; r++) {
+                var b = bricks[c][r];
+                if(b.status == 1) {
+                    if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+                        dy = -dy;
+                        b.status = 0;
+                        score++;
+                        if(score == brickRowCount*brickColumnCount) {
+                            alert("YOU WIN, CONGRATULATIONS!");
+                            document.location.reload();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    var bricks = [];
+    for(c=0; c<brickColumnCount; c++) {
+        bricks[c] = [];
+        for(r=0; r<brickRowCount; r++) {
+            bricks[c][r] = { x: 0, y: 0, status: 1};
+        }
+    }
+
+    function drawBall() {
+        ctx.beginPath();
+        ctx.arc(x, y, 10, 0, Math.PI*2);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+    }
+    
+    function draw() {
+        contador++;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawScore();
+        drawLives();
+        drawBall();
+        drawPaddle();
+        drawBricks();
+        if((y-ballRadius) + dy < 0) {
+            dy = -dy;
+        } else if((y+ballRadius) + dy > canvas.height) {
+            if(x > paddleX && x < paddleX + paddleWidth){
+                dy= -dy;
+            }else{
+                lives--;
+                if(!lives) {
+                    alert("GAME OVER");
+                    document.location.reload();
+                }
+                else {
+                    inicioJuego = true;
+                    x = canvas.width/2;y = canvas.height-paddleHeight-ballRadius;
+                    dx = 0; dy = 0;
+                    paddleX = (canvas.width-paddleWidth)/2;
+                }
+            }
+        }
+        if((x+ballRadius) + dx > canvas.width || (x-ballRadius) + dx < 0) {
+            dx = -dx;
+        }
+        
+        if(rightPressed && paddleX < canvas.width-paddleWidth) {
+            paddleX += 7;
+        }
+        else if(leftPressed && paddleX > 0) {
+            paddleX -= 7;
+        }
+        collisionDetection();
+        x += dx;
+        y += dy;
+
+        if(contador%1000 == 0){
+            addNewBricksRow();
+        }
+    }
+
+    function drawPaddle() {
+        ctx.beginPath();
+        ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+    }
+
+    function drawBricks() {
+        for(c=0; c<bricks.length; c++) {
+            for(r=0; r<bricks[c].length; r++) {
+                if(bricks[c][r].status == 1) {
+                    var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+                    var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+                    bricks[c][r].x = brickX;
+                    bricks[c][r].y = brickY;
+                    ctx.beginPath();
+                    ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                    ctx.fillStyle = "#0095DD";
+                    ctx.fill();
+                    ctx.closePath();
+                }
+            }
+        }
+    }
+
+    function addNewBricksRow(){
+        for(c=0; c<brickColumnCount; c++){
+            bricks[c].unshift({x:0, y:0, status:1})
+        }
+    }
+
+    function drawScore() {
+        ctx.font = "16px Arial";
+        ctx.fillStyle = "#0095DD";
+        ctx.fillText("Score: "+score, 8, 20);
+    }
+
+    function drawLives() {
+        ctx.font = "16px Arial";
+        ctx.fillStyle = "#0095DD";
+        ctx.fillText("Lives: "+lives, canvas.width-65, 20);
+    }
+
+    setInterval(draw,10);
+
+    
+}
